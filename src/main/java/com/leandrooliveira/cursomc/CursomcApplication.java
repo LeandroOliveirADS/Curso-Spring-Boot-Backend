9a9935d7e2e1db51da40cf1ec728c58d4e031cf3
Leandro Oliveira <leandroluis.cavalcantioliveira@gmail.com>
2019-06-23 17:56:51 -0300

    Pedido, EstadoPagamento, Pagamento
    aula 24

--- src/main/java/com/leandrooliveira/cursomc/CursomcApplication.java
+++ src/main/java/com/leandrooliveira/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.leandrooliveira.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.leandrooliveira.cursomc.domain.Cidade;
 import com.leandrooliveira.cursomc.domain.Cliente;
 import com.leandrooliveira.cursomc.domain.Endereco;
 import com.leandrooliveira.cursomc.domain.Estado;
+import com.leandrooliveira.cursomc.domain.Pagamento;
+import com.leandrooliveira.cursomc.domain.PagamentoComBoleto;
+import com.leandrooliveira.cursomc.domain.PagamentoComCartao;
+import com.leandrooliveira.cursomc.domain.Pedido;
 import com.leandrooliveira.cursomc.domain.Produto;
+import com.leandrooliveira.cursomc.domain.enums.EstadoPagamento;
 import com.leandrooliveira.cursomc.domain.enums.TipoCliente;
 import com.leandrooliveira.cursomc.repositories.CategoriaRepository;
 import com.leandrooliveira.cursomc.repositories.CidadeRepository;
 import com.leandrooliveira.cursomc.repositories.ClienteRepository;
 import com.leandrooliveira.cursomc.repositories.EnderecoRepository;
 import com.leandrooliveira.cursomc.repositories.EstadoRepository;
+import com.leandrooliveira.cursomc.repositories.PagamentoRepository;
+import com.leandrooliveira.cursomc.repositories.PedidoRepository;
 import com.leandrooliveira.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner{
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -85,6 +97,21 @@ public class CursomcApplication implements CommandLineRunner{
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 		
 	}
 
---
+++ src/main/java/com/leandrooliveira/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,34 @@
+package com.leandrooliveira.cursomc.domain;
+
+import javax.persistence.Entity;
+
+import com.leandrooliveira.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComCartao extends Pagamento {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {
+		
+	}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
+		super(id, estado, pedido);
+		this.setNumeroDeParcelas(numeroDeParcelas);
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+
+
+	
+	
+}
