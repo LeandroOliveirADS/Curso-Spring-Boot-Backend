f5e8b54e19d59eea56ee5b913701df35d3a81013
Leandro Oliveira <leandroluis.cavalcantioliveira@gmail.com>
2019-06-23 16:33:14 -0300

    endpoint /clientes/{id} disponivel
    aula 21

---
+++ src/main/java/com/leandrooliveira/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.leandrooliveira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leandrooliveira.cursomc.domain.Cliente;
+import com.leandrooliveira.cursomc.repositories.ClienteRepository;
+import com.leandrooliveira.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+		}
+}
