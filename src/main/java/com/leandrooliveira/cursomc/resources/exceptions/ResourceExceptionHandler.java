7b3ad754232d8aaf198d2f434beb72e2c283280f
Leandro Oliveira <leandroluis.cavalcantioliveira@gmail.com>
2019-06-29 20:40:25 -0300

    Deletando uma categoria com DELETE
    aula 33

--- src/main/java/com/leandrooliveira/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/leandrooliveira/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.leandrooliveira.cursomc.services.exceptions.DataIntegrityException;
 import com.leandrooliveira.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -18,5 +19,13 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(ObjectNotFoundException e, HttpServletRequest request){
+
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
 	
 }
--- src/main/java/com/leandrooliveira/cursomc/services/CategoriaService.java
+++ src/main/java/com/leandrooliveira/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.leandrooliveira.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.leandrooliveira.cursomc.domain.Categoria;
 import com.leandrooliveira.cursomc.repositories.CategoriaRepository;
+import com.leandrooliveira.cursomc.services.exceptions.DataIntegrityException;
 import com.leandrooliveira.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		try {
+			find(id);
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
 	}
 }
