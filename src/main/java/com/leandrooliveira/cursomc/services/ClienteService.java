b66a2b6023349d46a2d738137986a81031ea74e6
Leandro Oliveira <leandroluis.cavalcantioliveira@gmail.com>
2019-06-30 00:06:23 -0300

    PUT, DELETE, GET para Cliente
    aula 38

--- src/main/java/com/leandrooliveira/cursomc/services/ClienteService.java
+++ src/main/java/com/leandrooliveira/cursomc/services/ClienteService.java
@@ -1,12 +1,20 @@
 package com.leandrooliveira.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.leandrooliveira.cursomc.domain.Cliente;
+import com.leandrooliveira.cursomc.domain.Cliente;
+import com.leandrooliveira.cursomc.dto.ClienteDTO;
 import com.leandrooliveira.cursomc.repositories.ClienteRepository;
+import com.leandrooliveira.cursomc.services.exceptions.DataIntegrityException;
 import com.leandrooliveira.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,4 +28,38 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);		
+		return repo.save(newObj);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());		
+	}
+
+	public void delete(Integer id) {
+		try {
+			find(id);
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas.");
+		}		
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+
 }
