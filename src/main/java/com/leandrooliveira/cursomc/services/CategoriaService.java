f7ba20066fc4d5248b786a316ef4bd55c8ebe13f
Leandro Oliveira <leandroluis.cavalcantioliveira@gmail.com>
2019-06-29 23:24:38 -0300

    Validação sintática com Bean Validation
    aula 36

--- src/main/java/com/leandrooliveira/cursomc/services/CategoriaService.java
+++ src/main/java/com/leandrooliveira/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.leandrooliveira.cursomc.domain.Categoria;
+import com.leandrooliveira.cursomc.dto.CategoriaDTO;
 import com.leandrooliveira.cursomc.repositories.CategoriaRepository;
 import com.leandrooliveira.cursomc.services.exceptions.DataIntegrityException;
 import com.leandrooliveira.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
