b87add3692add842790dc06182f12fdf1cca56ad
Leandro Oliveira <leandroluis.cavalcantioliveira@gmail.com>
2019-06-29 21:04:59 -0300

    Listando todas Categorias com DTO
    aula 34

--- src/main/java/com/leandrooliveira/cursomc/services/CategoriaService.java
+++ src/main/java/com/leandrooliveira/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.leandrooliveira.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,10 @@ public class CategoriaService {
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
-		}
+		}		
+	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
 	}
 }
