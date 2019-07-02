d74228d249404b2fad7a5bc3c25402f3abef89c5
Leandro Oliveira <leandroluis.cavalcantioliveira@gmail.com>
2019-07-02 01:09:15 -0300

    Validação customizada de CPF ou CNPJ na inserção de Cliente
    aula 42

--- src/main/java/com/leandrooliveira/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/leandrooliveira/cursomc/dto/ClienteDTO.java
@@ -18,7 +18,7 @@ public class ClienteDTO implements Serializable{
 	@Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
 	
-	@NotEmpty
+	@NotEmpty(message = "Preenchimento obrigatório")
 	@Email(message = "Email inválido")
 	private String email;
 	
---
+++ src/main/java/com/leandrooliveira/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,38 @@
+package com.leandrooliveira.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.leandrooliveira.cursomc.domain.enums.TipoCliente;
+import com.leandrooliveira.cursomc.dto.ClienteNewDTO;
+import com.leandrooliveira.cursomc.resources.exceptions.FieldMessage;
+import com.leandrooliveira.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("CpfOuCnpj", "CPF Inválido"));
+		}
+		
+		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("CpfOuCnpj", "CNPJ Inválido"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
