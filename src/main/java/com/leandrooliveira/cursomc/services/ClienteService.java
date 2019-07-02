f39fee1ab488cd453cea3a8b1088f3612d92139d
Leandro Oliveira <leandroluis.cavalcantioliveira@gmail.com>
2019-07-01 22:20:23 -0300

    Ajustando e testando a deleção de Cliente
    aula 41

--- src/main/java/com/leandrooliveira/cursomc/services/ClienteService.java
+++ src/main/java/com/leandrooliveira/cursomc/services/ClienteService.java
@@ -57,7 +57,7 @@ public class ClienteService {
 			find(id);
 			repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas.");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionados.");
 		}		
 	}
 	
