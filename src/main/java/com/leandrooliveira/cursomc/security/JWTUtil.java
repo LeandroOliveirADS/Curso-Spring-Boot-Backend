29f6dec3f35ce50d23884f0e26165c50beffb0a2
Leandro Oliveira <leandroluis.cavalcantioliveira@gmail.com>
2019-11-10 15:46:41 -0300

    Implementando autenticacao e geracao do token JWT

---
+++ src/main/java/com/leandrooliveira/cursomc/security/JWTUtil.java
@@ -0,0 +1,27 @@
+package com.leandrooliveira.cursomc.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JWTUtil {
+
+	@Value("${jwt.secret}")
+	private String secret;
+	
+	@Value("${jwt.expiration}")
+	private Long expiration;
+	
+	public String generateToken(String userName) {
+		return Jwts.builder()
+				.setSubject(userName)
+				.setExpiration(new Date(System.currentTimeMillis() + expiration))
+				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
+				.compact();
+	}
+}
---
+++ src/main/java/com/leandrooliveira/cursomc/services/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.leandrooliveira.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.leandrooliveira.cursomc.domain.Cliente;
+import com.leandrooliveira.cursomc.repositories.ClienteRepository;
+import com.leandrooliveira.cursomc.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+@Autowired
+private ClienteRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+		
+		if(cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
