bc79f85dc69394e4b143ab5f790515da2b2318df
Leandro Oliveira <leandroluis.cavalcantioliveira@gmail.com>
2021-12-12 14:37:00 -0300

    Esqueci a senha

--- src/main/java/com/leandrooliveira/cursomc/config/SecurityConfig.java
+++ src/main/java/com/leandrooliveira/cursomc/config/SecurityConfig.java
@@ -38,7 +38,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		};
 	
 	private static final String[] PUBLIC_MATCHERS_POSTS = { 			  
-			  "/clientes/**"			  
+			  "/clientes/**",
+			  "/auth/forgot/**"
 		};
 
 	@Autowired
